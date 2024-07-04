284060dcfd95b7f1a00f56690baa480b55bf4f63
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-07-04 16:33:07 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,88 @@
+//require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from './db/db.js'
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function connectDB(){}
+connectDB() */
+
+/* 
+import express from 'express'
+const app = express()
+
+;(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", () => {
+        console.log("ERR: ", error)
+        throw error
+       })
+       app.listen(process.env.PORT,() => {
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})() */
