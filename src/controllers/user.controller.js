d5be64a329c9db4a34b65c7c007a3c026bb0e871
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-07-06 11:25:26 +0530

    so far setup is done multer and cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message: "All is well , I am not Aamir Khan"
-    })
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avtar
+    // upload them to cloudinary, avtar
+    // create user object - creation entry in db
+    // remove password and refresh token field from response
+    // check for user creation 
+    // if user created return response if not return error
+
+    const {fullname, email, username, password} = req.body
+    console.log("email", email)
+    console.log("password", password)
 })
 
 export {registerUser}
+
+// take usernamename, email, password, profile photo(optional), first save in the database, 2. give message saved to the user, 
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,51 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
+
+/*   // Configuration
+  cloudinary.config({ 
+    cloud_name: 'dn5bukzhm', 
+    api_key: '631686147955652', 
+    api_secret: '<your_api_secret>' // Click 'View Credentials' below to copy your API secret
+});
+
+// Upload an image
+ const uploadResult = await cloudinary.uploader
+   .upload(
+       'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+           public_id: 'shoes',
+       }
+   ) 
+   .catch((error) => {
+       console.log(error);
+   }); */
