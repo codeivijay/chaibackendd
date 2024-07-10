8d93c3cf92ccd4a91d4f7a1a4ab76b6cadbea374
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-07-10 18:24:37 +0530

    get user bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -255,7 +255,7 @@ const upadateAccountDetails = asyncHandler(async(req, res) => {
     throw new ApiError(400, "All fields are required")
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
